66cb903f1a5cbf46216a825582d0b08af9a70072
Farooq Azam Khan <khanazamfarooq99@gmail.com>
2022-01-03 22:37:11 -0500

    fizzbuzz 3

--- src/FizzBuzz.elm
+++ src/FizzBuzz.elm
@@ -1,4 +1,14 @@
 module FizzBuzz exposing (fizzbuzz)
 
 fizzbuzz : Int -> String
-fizzbuzz x = ""
+fizzbuzz n =
+    List.range 1 n
+    |> List.map is_x_fizz_buzz 
+    |> String.join "\n"
+
+is_x_fizz_buzz : Int -> String
+is_x_fizz_buzz x = 
+    if modBy 3 x == 0 
+        then "Fizz"
+        else String.fromInt x
+
--- tests/Example.elm
+++ tests/Example.elm
@@ -4,8 +4,15 @@ import Expect exposing (Expectation)
 import Fuzz exposing (Fuzzer, int, list, string)
 import Test exposing (..)
 
-import FizzBuzz 
+import FizzBuzz exposing (fizzbuzz)
 
 suite : Test
 suite =
-    todo "Implement our first test. See https://package.elm-lang.org/packages/elm-explorations/test/latest for how to do this!"
+    describe "Fizzbuzz tdd"
+    [ test "fizzbuzz 1" <| 
+        \_ -> Expect.equal (fizzbuzz 1) "1"
+    , test "fizzbuzz 2" <|
+        \_ -> Expect.equal (fizzbuzz 2) "1\n2"
+    , test "fizzbuzz 3" <|
+        \_ -> Expect.equal (fizzbuzz 3) "1\n2\nFizz"
+    ]
